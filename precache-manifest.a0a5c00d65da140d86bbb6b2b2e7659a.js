b181a4aee1d9aa7a46baaeb2807528be79872898
mohankumar27 <mohankumar27197@gmail.com>
2020-08-16 20:58:56 +0530

    Updates

---
+++ precache-manifest.a0a5c00d65da140d86bbb6b2b2e7659a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13d24110c5259a1a609c70c521c00653",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "6d9614cecd37ef222307",
+    "url": "/Covid-19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "62462440cf02458e8ea2",
+    "url": "/Covid-19-Tracker/static/css/main.9e632c69.chunk.css"
+  },
+  {
+    "revision": "6d9614cecd37ef222307",
+    "url": "/Covid-19-Tracker/static/js/2.6aec1410.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Tracker/static/js/2.6aec1410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62462440cf02458e8ea2",
+    "url": "/Covid-19-Tracker/static/js/main.91f26df1.chunk.js"
+  },
+  {
+    "revision": "4b644fa9ca0287604fad",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.b2c8fe1b.js"
+  }
+]);
